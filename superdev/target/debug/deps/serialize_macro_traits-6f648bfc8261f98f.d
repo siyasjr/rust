10ee8488da9707feb39305433313a99e98385c78
siyasjr <siyasjr@gmail.com>
2025-08-14 23:37:25 +0530

    new library crate called serialise_macro_traits

---
+++ superdev/target/debug/deps/serialize_macro_traits-6f648bfc8261f98f.d
@@ -0,0 +1,5 @@
+/home/siy/rust/superdev/target/debug/deps/serialize_macro_traits-6f648bfc8261f98f.d: serialize_macro_traits/src/lib.rs
+
+/home/siy/rust/superdev/target/debug/deps/libserialize_macro_traits-6f648bfc8261f98f.rmeta: serialize_macro_traits/src/lib.rs
+
+serialize_macro_traits/src/lib.rs:
