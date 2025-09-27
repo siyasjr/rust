f5bca6c1b314dc6df6ce25bfd2b6012c6fd8bfed
siyasjr <siyasjr@gmail.com>
2025-09-27 23:57:47 +0530

    print name

--- rust-recap/src/main.rs
+++ rust-recap/src/main.rs
@@ -1,4 +1,26 @@
 
-fn main(){
-    println!("Hellow world");
+fn main() {
+
+ print_name();
+
+ 
+
+
+}
+
+
+
+
+
+fn print_name() {
+    let name: &str = "Nvidia";
+    let mut age: u32 = 25;
+
+
+    println!("The name is {}", name);
+    println!("The age is {}", age);
+
+    age += 1 ;
+
+    println!("Your age after this year is {}", age);
 }
