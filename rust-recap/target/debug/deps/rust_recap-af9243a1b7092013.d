20c4de621fb3ebf8c6fc2dfa39fdd05e9b43a0b8
siyasjr <siyasjr@gmail.com>
2025-10-12 23:36:56 +0530

    associiated files

---
+++ rust-recap/target/debug/deps/rust_recap-af9243a1b7092013.d
@@ -0,0 +1,5 @@
+/home/siy/rust/rust-recap/target/debug/deps/rust_recap-af9243a1b7092013.d: src/main.rs
+
+/home/siy/rust/rust-recap/target/debug/deps/librust_recap-af9243a1b7092013.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust-recap/target/debug/deps/rust_recap-fdb5dc0225d25b12.d
@@ -0,0 +1,5 @@
+/home/siy/rust/rust-recap/target/debug/deps/rust_recap-fdb5dc0225d25b12.d: src/main.rs
+
+/home/siy/rust/rust-recap/target/debug/deps/librust_recap-fdb5dc0225d25b12.rmeta: src/main.rs
+
+src/main.rs:
